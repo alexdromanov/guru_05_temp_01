075a6387a9cf4f38bcfe58594768f2d61451ecbc
AD <dentist@rambler.ru>
2021-04-02 13:41:58 +0300

    Second + SelenoinTest

---
+++ src/test/java/myallure/SelenideTest.java
@@ -0,0 +1,36 @@
+package myallure;
+
+
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+    public class SelenideTest {
+        private final String BASE_URL = "https://github.com";
+        private final static String repName = "alexdromanov/guru_05_temp_01";
+        private final static String findValue = "Issue";
+
+        @Test
+        @DisplayName("Селенидовский логгер")
+        public void testIssueSearch() {
+
+            SelenideLogger.addListener("allure", new AllureSelenide());
+            open(BASE_URL);
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys(repName);
+            $(".header-search-input").submit();
+            $(By.linkText(repName)).click();
+            $(withText("Issues")).click();
+            $(withText(findValue)).should(Condition.exist);
+        }
+
+    }
+
